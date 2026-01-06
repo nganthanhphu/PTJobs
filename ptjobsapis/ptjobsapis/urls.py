d7a027192764d84b61c5c22cfe36bab8c066080c
Marcoh05P <marcoh.dev@outlook.com>
2026-01-06 20:36:16 +0700

    feat: Update info

--- ptjobsapis/ptjobsapis/urls.py
+++ ptjobsapis/ptjobsapis/urls.py
@@ -3,32 +3,33 @@ from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from ptjobs.admin import admin_site
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="PTJobs API",
-        default_version='v1',
-        description="APIs for PTJobs App",
-        contact=openapi.Contact(email="nganthanhphu@ou.edu.vn"),
-        license=openapi.License(name="Ngàn Thành Phú@2025"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny]
-)
+        openapi.Info(
+                title="PTJobs API",
+                default_version='v1',
+                description="APIs for PTJobs App",
+                contact=openapi.Contact(email="@ou.edu.vn"),
+                license=openapi.License(name="@2025"),
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny]
+        )
 
 urlpatterns = [
-    path('', include('ptjobs.urls')),
-    path('admin/', admin_site.urls),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0),
-            name='schema-json'),
-    re_path(r'^swagger/$',
-            schema_view.with_ui('swagger', cache_timeout=0),
-            name='schema-swagger-ui'),
-    re_path(r'^redoc/$',
-            schema_view.with_ui('redoc', cache_timeout=0),
-            name='schema-redoc'),
-    path('o/', include('oauth2_provider.urls',
-                       namespace='oauth2_provider')),
+        path('', include('ptjobs.urls')),
+        path('admin/', admin_site.urls),
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+                schema_view.without_ui(cache_timeout=0),
+                name='schema-json'),
+        re_path(r'^swagger/$',
+                schema_view.with_ui('swagger', cache_timeout=0),
+                name='schema-swagger-ui'),
+        re_path(r'^redoc/$',
+                schema_view.with_ui('redoc', cache_timeout=0),
+                name='schema-redoc'),
+        path('o/', include('oauth2_provider.urls',
+                        namespace='oauth2_provider')),
 
-]
+        ]
